eaa9e7daca9f5e63cccfac7eee32a59ccc9a90e5
liuchenmaths <80818536+liuchenmaths@users.noreply.github.com>
2021-03-23 16:50:15 +0800

    013课更新

--- python test/test.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-
-content = '北 京 欢 迎 你 为 你 开 天 辟 地           '
-while True:
-    os.system('clear')
-    print(content)
-    time.sleep(0.1)
-    content = content[1:] + content[0]
